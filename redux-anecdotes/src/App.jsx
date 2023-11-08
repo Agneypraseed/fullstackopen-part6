fcdb0842754651f5f0017eda34fa84928eecb97d
Agney Praseed <agneysince2000@gmail.com>
2023-11-09 00:38:21 +0530

    6.3: anecdotes, step1

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -5,7 +5,13 @@ const App = () => {
   const dispatch = useDispatch()
 
   const vote = (id) => {
-    console.log('vote', id)
+    console.log("clicked vote");
+    dispatch({
+      type: 'VOTE',
+      payload: {        
+        id: id
+      }
+    })
   }
 
   return (
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,11 +19,19 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject)
 
-const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+const reducer = (state = initialState, action) => {   
+  switch (action.type) {
+    case "VOTE":
+      const id = action.payload.id;
+      const anecdoteToChange = state.find((n) => n.id === id);         
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      };      
+      return state.map((anecdote) => (anecdote.id !== id ? anecdote : changedAnecdote));    
+    default:
+      return state;
+  }  
 }
 
 export default reducer
