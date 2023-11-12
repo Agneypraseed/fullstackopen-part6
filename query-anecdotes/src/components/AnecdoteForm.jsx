7f8de4f7d6d47e650cfaefcb771f5abd9461fbf5
Agney Praseed <agneysince2000@gmail.com>
2023-11-12 22:03:15 +0530

    Exercise 6.21

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,36 @@
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { createAnecdote } from "../server/requests";
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient();
+
+  const newNoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData({ queryKey: ["anecdotes"] });
+      queryClient.setQueryData(
+        { queryKey: ["anecdotes"] },
+        anecdotes.concat(newAnecdote)
+      );
+    },
+  });
 
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    newNoteMutation.mutate({ content, votes: 0 });
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
